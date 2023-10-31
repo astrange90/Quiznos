a7646577cd44cb0dca9c379768d14bf22ea03079
astrange90 <astrange90@hotmail.com>
2023-10-30 21:27:24 -0700

    added css styles, stuck on js code

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
+var startButton = document.querySelector(".start-button");
+var nextBtn = document.getElementById("next");
+var numberofQuestions = document.querySelector(".question-amount")
+var isCorrect = false;
 
 function countdown() {
-    var timeLeft = 300;
+    var timeLeft = 60;
   
     
     var timeInterval = setInterval(function () {
 
-      if (timeLeft > 1) {
+      if (timeLeft >= 0) {
         timerEl.textContent = timeLeft + ' seconds remaining';
         timeLeft--;
       } else if (timeLeft === 1) {
         timerEl.textContent = timeLeft + ' second remaining';
         timeLeft--;
       } else {
-        timerEl.textContent = '';
+        timerEl.textContent = 'Time is Up!';
     
         clearInterval(timeInterval);
         displayMessage();
@@ -22,4 +26,23 @@ function countdown() {
     }, 1000);
   }
 
+function renderBlanks() {
+
+}
+
+function startQuiz() {
+    isCorrect = false;
+    timerCount = 60;
+    startButton.disabled = true;
+    renderB
+
+
+}
+
+next.addEventListener("click", function(event){
+    event.stopPropagation();
+    
+    NavigationPreloadManager(1);
+})
+  startButton.addEventListener("click", startQuiz);
   countdown();
